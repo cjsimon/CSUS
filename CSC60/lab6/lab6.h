0de79ae51a19c1f5ff77b188747d92d15de51852
Christopher Simon <cjsimon333@gmail.com>
2016-08-23 02:26:45 -0700

    Added remaining projects from spr2016

--- CSC60/lab6/lab6.h
+++
@@ -1,5 +0,0 @@
-/* lab6.h             */
-/* function prototypes */
-double semi_perimeter(double a, double b, double c);	    
-double radius_inside_circle(double a, double b, double c);		
-double radius_outside_circle(double a, double b, double c); 
