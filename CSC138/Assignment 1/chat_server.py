d92c4cae9d109ffff9c58b50941a12ca7c639287
Christopher <cjsimon333@gmail.com>
2016-10-27 21:24:31 -0700

    Added udp server and client

---
+++ CSC138/Assignment 1/chat_server.py
@@ -0,0 +1,73 @@
+import socket
+import select
+
+# Broadcasts data to all other connected clients in the server,
+# not including the broadcasting client
+def broadcast(broadcastingClient, data):
+	for client in clients:
+		# Do not broadcast the data to the server
+		# or the broadcasting client itself
+		if client == server or client == broadcastingClient:
+			pass
+		try:
+			# Send data to the current client
+			client.send(data)
+		except:
+			# If there is an error, close the current client
+			# and remove it from the list. It was probably disconnected
+			client.close()
+			clients.remove(client)
+ 
+if __name__ == '__main__': 
+	# The list of connected clients, including the server
+	clients = []
+	
+	# 0.0.0.0 is both localhost and the ip of the computer
+	ip = '0.0.0.0'
+	port = 8000
+	
+	# Create a server socket
+	server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	
+	# Bind the socket to the ip of the server
+	server.bind((ip, port))
+	
+	# Listen for up to 10 client connections
+	server.listen(10)
+	
+	# Add the server to the list of clients
+	clients.append(server)
+	print("Listening on port %i" % port)
+	
+	# Keep the server running...
+	while True:
+		# Get the list of file descriptors for each of the sockets
+		# select gets the read, write, and error file descriptors
+		socketsOut, socketsIn, socketsError = select.select(clients, [], [])
+		
+		for socket in socketsOut:
+			if socket == server:
+				# Check for new connections
+				client, clientAddress = server.accept()
+				clients.append(client)
+				print "Client Connected: (%s, %s)" % clientAddress
+				broadcast(client, "Client has entered room: (%s:%s)\n" % clientAddress)
+			#Some incoming message from a client
+			else:
+				try:
+					# Get data from the current client socket
+					data = socket.recv(4096)
+					# If any data was received, broadcast it to all other clients
+					if data:
+						broadcast(socket, '\r%s: %s' % str(socket.getpeername()), data)
+				except:
+					disconnectMessage = "Client (%s, %s) has disconnected" % clientAddress
+					# Indicate that the client has been disconnected
+					broadcast(socket, disconnectMessage)
+					print(disconnectMessage)
+					# Close the client connection and remove it from the list of connected clients
+					socket.close()
+					clients.remove(socket)
+					# This client no longer exists. Goto the next one
+					continue
+	server.close()
--- CSC138/Assignment 1/udp_client.py
+++ CSC138/Assignment 1/udp_client.py
@@ -3,13 +3,10 @@ import sys
 
 if __name__ == "__main__":
 	# Create a client socket    
-	client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 	
 	# Bind the socket to localhost on port 8000
 	ip, port = '127.0.0.1', 8000
-	
-	# Connect the client to the host on port
-	client.connect((ip, port))
 	
 	# Allow the client to send and recieve messages
 	# to and from the server so long as the client
@@ -17,10 +14,10 @@ if __name__ == "__main__":
 	clientData = ''
 	while(clientData.lower() != 'exit'):
 		clientData = raw_input('Type a message to send to the server: ')
-		client.send(clientData)
+		client.sendto(clientData, (ip, port))
 		
 		# Get the responce data from the server 
-		serverData = client.recv(1024)
+		serverData, serverAddress = client.recvfrom(1024)
 		print('Server Response: %s' % serverData)
 	
 	client.close()
